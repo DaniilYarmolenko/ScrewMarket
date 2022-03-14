4d237948c7e2cce051d8d1cf6be55793c175a744
Daniil Yarmolenko <DaniilYarmolenko@gmail.com>
2022-03-14 22:35:28 +0300

    Start Project
    StartProject

---
+++ ScrewMarket/ScrewMarketUITests/ScrewMarketUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ScrewMarketUITestsLaunchTests.swift
+//  ScrewMarketUITests
+//
+//  Created by Даниил Ярмоленко on 14.03.2022.
+//
+
+import XCTest
+
+class ScrewMarketUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
